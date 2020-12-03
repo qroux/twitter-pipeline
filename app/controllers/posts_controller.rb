046bcb45532b5a9feaeb7577863590fa4b3c10f6
quentin roux <quentin.roux@hotmail.fr>
2020-12-03 18:27:52 +0100

    form working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,27 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      flash[:alert] = "Missing field"
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :official_url,
+      :official_photo,
+      :opposite_url,
+      :opposite_photo)
   end
 end
